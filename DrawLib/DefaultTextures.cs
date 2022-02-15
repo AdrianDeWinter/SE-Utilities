4d2f8db800c2d9e322cbd3b005476193f70794cf
Martin Blecker <martinblecker@gmx.de>
2022-02-15 14:10:10 +0100

    Changed the A default texture from struct initializer over to sprite constructor, one readable examble (smiley) is suffcient and more are are a waste of PB script lines

--- DrawLib/DefaultTextures.cs
+++ DrawLib/DefaultTextures.cs
@@ -23,7 +23,7 @@ namespace IngameScript
 	partial class Program
 	{
 		/// <summary>
-		/// This static class holds a number of defaukt textures, for both demonstration purposes and as useful building blocks.
+		/// This static class holds a number of default textures, for both demonstration purposes and as useful building blocks.
 		/// TODO: add the entire SE font as textures, so they can rotate and scale like normal sprites
 		/// </summary>
 		public static class DefaultTextures
@@ -78,86 +78,14 @@ namespace IngameScript
 			/// Letter 'A' in SE's font
 			/// </summary>
 			public static Texture A = new Texture(_name: "A", _width: 260, _height: 185, _sprites: new List<MySprite>{
-					new MySprite()
-					{
-						Type = SpriteType.TEXTURE,
-						Alignment = TextAlignment.CENTER,
-						Data = "SquareSimple",
-						Position = new Vector2(98f,0),
-						Size = new Vector2(35f,150f),
-						Color = new Color(0,0,0,255),
-						RotationOrScale = -0.192f
-					}, // Right Leg
-					new MySprite()
-					{
-						Type = SpriteType.TEXTURE,
-						Alignment = TextAlignment.CENTER,
-						Data = "SquareSimple",
-						Position = new Vector2(-98f,0f),
-						Size = new Vector2(35f,150f),
-						Color = new Color(0,0,0,255),
-						RotationOrScale = 0.192f
-					}, // Left Leg
-					new MySprite()
-					{
-						Type = SpriteType.TEXTURE,
-						Alignment = TextAlignment.CENTER,
-						Data = "SquareSimple",
-						Position = new Vector2(0f,-75f),
-						Size = new Vector2(160f,35f),
-						Color = new Color(0,0,0,255),
-						RotationOrScale = 0f
-					}, // Top Bar
-					new MySprite()
-					{
-						Type = SpriteType.TEXTURE,
-						Alignment = TextAlignment.CENTER,
-						Data = "SquareSimple",
-						Position = new Vector2(0f, -5f),
-						Size = new Vector2(190f, 35f),
-						Color = new Color(0, 0, 0, 255),
-						RotationOrScale = 0f
-					}, // Middle Bar
-					new MySprite()
-					{
-						Type = SpriteType.TEXTURE,
-						Alignment = TextAlignment.CENTER,
-						Data = "Circle",
-						Position = new Vector2(113f, 75f),
-						Size = new Vector2(35f, 35f),
-						Color = new Color(0, 0, 0, 255),
-						RotationOrScale = 0f
-					}, // Bottom Right Corner
-					new MySprite()
-					{
-						Type = SpriteType.TEXTURE,
-						Alignment = TextAlignment.CENTER,
-						Data = "Circle",
-						Position = new Vector2(-113f, 75f),
-						Size = new Vector2(35f, 35f),
-						Color = new Color(0, 0, 0, 255),
-						RotationOrScale = 0f
-					}, // Bottom Left Corner
-					new MySprite()
-					{
-						Type = SpriteType.TEXTURE,
-						Alignment = TextAlignment.CENTER,
-						Data = "Circle",
-						Position = new Vector2(-83f, -75f),
-						Size = new Vector2(35f, 35f),
-						Color = new Color(0, 0, 0, 255),
-						RotationOrScale = 0f
-					}, // Top Left Corner
-					new MySprite()
-					{
-						Type = SpriteType.TEXTURE,
-						Alignment = TextAlignment.CENTER,
-						Data = "Circle",
-						Position = new Vector2(83f, -75f),
-						Size = new Vector2(35f, 35f),
-						Color = new Color(0, 0, 0, 255),
-						RotationOrScale = 0f
-					} // Top Right Corner
+				new MySprite(SpriteType.TEXTURE, "SquareSimple", new Vector2(98f,0f), new Vector2(35f,150f), new Color(0,0,0,255), null, TextAlignment.CENTER, -0.192f), // Right Leg
+				new MySprite(SpriteType.TEXTURE, "SquareSimple", new Vector2(-98f,0f), new Vector2(35f,150f), new Color(0,0,0,255), null, TextAlignment.CENTER, 0.192f), // Left Leg
+				new MySprite(SpriteType.TEXTURE, "SquareSimple", new Vector2(0f,-75f), new Vector2(160f,35f), new Color(0,0,0,255), null, TextAlignment.CENTER, 0f), // Top Bar
+				new MySprite(SpriteType.TEXTURE, "SquareSimple", new Vector2(0f,-5f), new Vector2(190f,35f), new Color(0,0,0,255), null, TextAlignment.CENTER, 0f), // Middle Bar
+				new MySprite(SpriteType.TEXTURE, "Circle", new Vector2(113f,75f), new Vector2(35f,35f), new Color(0,0,0,255), null, TextAlignment.CENTER, 0f), // Bottom Right Corner
+				new MySprite(SpriteType.TEXTURE, "Circle", new Vector2(-113f,75f), new Vector2(35f,35f), new Color(0,0,0,255), null, TextAlignment.CENTER, 0f), // Bottom Left Corner
+				new MySprite(SpriteType.TEXTURE, "Circle", new Vector2(-83f,-75f), new Vector2(35f,35f), new Color(0,0,0,255), null, TextAlignment.CENTER, 0f), // Top Left Corner
+				new MySprite(SpriteType.TEXTURE, "Circle", new Vector2(83f,-75f), new Vector2(35f,35f), new Color(0,0,0,255), null, TextAlignment.CENTER, 0f) // Top Right Corner
 			});
 			/// <summary>
 			/// Letter 'B' in SE's font
@@ -173,7 +101,6 @@ namespace IngameScript
 						new MySprite(SpriteType.TEXTURE, "SemiCircle", new Vector2(71f,-35f), new Vector2(115f,115f), new Color(0,0,0,255), null, TextAlignment.CENTER, 1.5708f), // Top Circle
 						new MySprite(SpriteType.TEXTURE, "SemiCircle", new Vector2(71f,37f), new Vector2(40f,40f), new Color(255,255,255,255), null, TextAlignment.CENTER, 1.5708f), // Bottom Circle Cutout
 						new MySprite(SpriteType.TEXTURE, "SemiCircle", new Vector2(71f,-37f), new Vector2(40f,40f), new Color(255,255,255,255), null, TextAlignment.CENTER, 1.5708f) // Top Circle Cutout
-
 			});
 		}
 	}
